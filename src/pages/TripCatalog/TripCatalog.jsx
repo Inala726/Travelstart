9c0502a9a9ec30d27321d8155bf693e953449276
Daniel24356 <igwedaniel128@gmail.com>
2024-09-17 21:16:31 -0700

    Daniel is tired

--- src/pages/TripCatalog/TripCatalog.jsx
+++ src/pages/TripCatalog/TripCatalog.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./trip.css"
 import Vlog from "../../assets/header.jpg"
+import VBeach from "../../assets/beach-park.jpg"
 
 const TripCatalog = () => {
   return (
@@ -18,8 +19,127 @@ const TripCatalog = () => {
             </div>
         </div>
         <div className="tripContent">
-            
+            <div className='beach-all'>
+              <div className='beach-div'>
+                <img src={VBeach} alt="" />
+                <div className='dark-comment'>
+                <div>
+                  <p>Tour operators retooling</p>
+                  <p>Itineraries, looking for 2022</p>
+                </div>
+                <div className='comment-div'>
+                  <p>July 15 2021</p>
+                  <p>No comments</p>
+                </div>
+                </div>
+              
+              </div>
+
+             <div className='beach-div'>
+                <img src={VBeach} alt="" />
+                <div className='dark-comment'>
+                <div>
+                  <p>Tour operators retooling</p>
+                  <p>Itineraries, looking for 2022</p>
+                </div>
+                <div className='comment-div'>
+                  <p>July 15 2021</p>
+                  <p>No comments</p>
+                </div>
+                </div>
+              
+              </div>
+
+               <div className='beach-div'>
+                <img src={VBeach} alt="" />
+                <div className='dark-comment'>
+                <div>
+                  <p>Tour operators retooling</p>
+                  <p>Itineraries, looking for 2022</p>
+                </div>
+                <div className='comment-div'>
+                  <p>July 15 2021</p>
+                  <p>No comments</p>
+                </div>
+                </div>
+              
+              </div>
+            </div>
+
+            <div className='tour-all'>
+              <div className='tour-div'>
+                <img className='' src={VBeach} alt="" />
+                <div className='bruck-div'>
+                <p>Inns Bruck Tourismus introduces worm card to</p>
+                <p>travelers</p>
+                <div className='comment-div'>
+                  <p>July 15 2021</p>
+                  <p>No comments</p>
+                </div>
+                </div>               
+              </div>
+
+              <div className='tour-div'>
+                <img className='' src={VBeach} alt="" />
+                <div className='bruck-div'>
+                <p>Inns Bruck Tourismus introduces worm card to</p>
+                <p>travelers</p>
+                <div className='comment-div'>
+                  <p>July 15 2021</p>
+                  <p>No comments</p>
+                </div>
+                </div>               
+              </div>
+            </div>
         </div>
+
+        <div className='beach-all'>
+              <div className='beach-div'>
+                <img src={VBeach} alt="" />
+                <div className='dark-comment'>
+                <div>
+                  <p>Tour operators retooling</p>
+                  <p>Itineraries, looking for 2022</p>
+                </div>
+                <div className='comment-div'>
+                  <p>July 15 2021</p>
+                  <p>No comments</p>
+                </div>
+                </div>
+              
+              </div>
+
+             <div className='beach-div'>
+                <img src={VBeach} alt="" />
+                <div className='dark-comment'>
+                <div>
+                  <p>Tour operators retooling</p>
+                  <p>Itineraries, looking for 2022</p>
+                </div>
+                <div className='comment-div'>
+                  <p>July 15 2021</p>
+                  <p>No comments</p>
+                </div>
+                </div>
+              
+              </div>
+
+               <div className='beach-div'>
+                <img src={VBeach} alt="" />
+                <div className='dark-comment'>
+                <div>
+                  <p>Tour operators retooling</p>
+                  <p>Itineraries, looking for 2022</p>
+                </div>
+                <div className='comment-div'>
+                  <p>July 15 2021</p>
+                  <p>No comments</p>
+                </div>
+                </div>
+              
+              </div>
+            </div>
+            
     </div>
       
     </>
