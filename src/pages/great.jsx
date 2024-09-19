08778e510b4447f12a8d98da28304de3df88f9e1
Daniel24356 <igwedaniel128@gmail.com>
2024-09-19 01:58:59 -0700

    We chill

--- src/pages/great.jsx
+++ src/pages/great.jsx
@@ -1,6 +1,18 @@
 import './great.css'
 import Vmount from "../assets/mountain-img.jpg"
 import Vresort from "../assets/house-resort.jpg"
+import Vgif from "../assets/giphy.webp"
+import Vbus from "../assets/giphy2.webp"
+import Vflight from "../assets/giphy3.webp"
+import Vflight2 from "../assets/giphy4.webp"
+import Vimg1 from "../assets/1-img.jpg"
+import Vimg2 from "../assets/2-img.jpg"
+import Vimg3 from "../assets/3-img.jpg"
+import Vimg4 from "../assets/4-img.jpg"
+import Vimg5 from "../assets/5-img.jpg"
+import Vimg6 from "../assets/6-img.jpg"
+import Vimg7 from "../assets/7-img.jpg"
+// import Vimg1 from "../assets/1-img.jpg"
 
 const Great = () => {
     return (
@@ -13,92 +25,178 @@ const Great = () => {
             backgroundPosition: "bottom"
             }}>
                 <div className='overlays'>
-                <h1>The Great Outdoors</h1>
+                <h1>Tour Around Asia</h1>
                 <p>Wander often, Wonder Always </p>
                 </div>
                
             </section>
+
+             <section className='sec-gal'>
+                <h1 className='pop-txt'>Popular Countries</h1>
+                <div className='box-all'>
+                <div className='first-box'>
+                    <div  style={{
+            backgroundImage: `url(${Vimg1})`,
+            backgroundRepeat: "none",
+            backgroundSize: "cover",
+            backgroundPosition: "bottom"
+            }}>
+                        <h1>HAMBURG</h1>
+                        <p>220 locations</p>
+                    </div>
+                    <div  style={{
+            backgroundImage: `url(${Vimg2})`,
+            backgroundRepeat: "none",
+            backgroundSize: "cover",
+            backgroundPosition: "bottom"
+            }}>
+                        <h1>KOLN</h1>
+                        <p>220 locations</p>
+                    </div>
+                </div>
+                <div className='second-box'>
+                    <div  style={{
+            backgroundImage: `url(${Vimg3})`,
+            backgroundRepeat: "none",
+            backgroundSize: "cover",
+            backgroundPosition: "bottom"
+            }}>
+                        <h1>Berlin</h1>
+                        <p>220 locations</p>
+                    </div>
+                    <div  style={{
+            backgroundImage: `url(${Vimg4})`,
+            backgroundRepeat: "none",
+            backgroundSize: "cover",
+            backgroundPosition: "bottom"
+            }}>
+                    <h1>DUSSELDORF</h1>
+                    <p>220 locations</p>
+                    </div>
+                    <div  style={{
+            backgroundImage: `url(${Vimg5})`,
+            backgroundRepeat: "none",
+            backgroundSize: "cover",
+            backgroundPosition: "bottom"
+            }}>
+                    <h1>FRANKFURT</h1>
+                    <p>220 locations</p>
+                    </div>
+                </div>
+                <div className='third-box'>
+                    <div  style={{
+            backgroundImage: `url(${Vimg6})`,
+            backgroundRepeat: "none",
+            backgroundSize: "cover",
+            backgroundPosition: "bottom"
+            }}>
+                        <h1>MUNCHEN</h1>
+                        <p>220 locations</p>
+                    </div>
+                    <div  style={{
+            backgroundImage: `url(${Vimg7})`,
+            backgroundRepeat: "none",
+            backgroundSize: "cover",
+            backgroundPosition: "bottom"
+            }}>
+                        <h1>HANNOVER</h1>
+                        <p>220 locations</p>
+                    </div>
+                </div>
+                </div>
+             </section>
+
             <section className='explore-div'>
                 <div className='world-div'>
-                    <h1>Explore the world</h1>
+                    <h1>Available Transports</h1>
                     <div>
-                        <p>We work to provide the most content from athletes </p>
-                        <p>Explorers and travelers from around the world, our longterm messages is to educate </p>
-                        <p>require and enable all people to aquintanance and wildlife</p>
+                        <p>We work to provide transport means for everyone</p>
                     </div>
                 </div>
 
                 <div className='locations-div'>
                     <div className='bck-image' style={{
-            backgroundImage: `url(${Vresort})`,
+            backgroundImage: `url(${Vflight2})`,
             backgroundRepeat: "none",
             backgroundSize: "cover",
             backgroundPosition: "bottom"
             }}>     
-                    <p>NaeruyJordon</p>  
-                    <p> Norway</p>
-                       
+                    <p className='trans-txt'>Flight</p>  
+                    <p className='see-txt'>see more</p>
                     </div>
                     <div className='bck-image' style={{
-            backgroundImage: `url(${Vresort})`,
+            backgroundImage: `url(${Vgif})`,
             backgroundRepeat: "none",
             backgroundSize: "cover",
             backgroundPosition: "bottom"
             }}>     
-                    <p>NaeruyJordon</p>  
-                    <p> Norway</p>
+                  <p className='trans-txt'>Cars</p>  
+                  <p className='see-txt'>see more</p>
                        
                     </div>
 
                  <div className='bck-image' style={{
-            backgroundImage: `url(${Vresort})`,
+            backgroundImage: `url(${Vbus})`,
             backgroundRepeat: "none",
             backgroundSize: "cover",
             backgroundPosition: "bottom"
             }}>     
-                    <p>NaeruyJordon</p>  
-                    <p> Norway</p>
+                   <p className='trans-txt'>Bus</p>  
+                   <p className='see-txt'>see more</p>
                        
                     </div>
 
                 </div>
-                <p className='seemore-txt'>see more</p>
-            </section>
-
-            <section className='explore-divs'>
-                <div className='world-div'>
-                <h1>The Journal</h1>
-                <p>Our favourite stories about public places
-                 and opportunity for you to get</p>
-                <p>involved in protecting your outdoor appearances</p>
-                </div>
-                <div className='nat-all'>
-                    <div className='unforget-div'>
-                        <img src={Vresort} alt="" />
-                        <p>May 30, 2007</p>
-                        <h1>An unforgettable</h1>
-                        <div>
-                            <p>If you have only day to vast Karmine National Park</p>
-                            <p>and you want to make the most out of it</p>
-                        </div>
-                    </div>
-
-                    <div className='unforget-div'>
-                        <img src={Vresort} alt="" />
-                        <p>May 30, 2007</p>
-                        <h1>An unforgettable</h1>
-                        <div>
-                            <p>If you have only day to vast Karmine National Park</p>
-                            <p>and you want to make the most out of it</p>
-                        </div>
-                    </div>
-
-                </div>
-                <p>All Posts</p>
+                {/* <p className='seemore-txt'>see more</p> */}
             </section>
 
             <section>
+              <h1>Activities to do In Asia</h1>
+              <div>
+                <div>
+                    <p>DAY TRIPS & EXCURSIONS</p>
+                    <p>TOURS & SIGHTSEEING</p>
+                    <p>Reservations</p>
+                    <p>CULTURAL & THEME TOURS</p>
+                </div>
+                <div>
+                    <div>
+                        <p>SHOW ONLY</p>
+                        <div>CULTURAL TOURS</div>
+                    </div>
+                    <div>
+                        <button>VIEW ALL</button>
+                    </div>
+                </div>
+              </div>
 
+              <div>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                        <div>
+                            <div>
+                                <div>
+                                    <p>OAHU DAY TRIP</p>
+                                    <p>MAUI,HAWAII</p>
+                                </div>
+                                <img src="" alt="" />
+                            </div>
+                            <p>Oahu Day Trip to Pearl Harbor, Honolulu</p>
+                            <p>and Punchbowl </p>
+                        </div>
+                    </div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                     <div></div>
+                     <div></div>
+                     <div></div>
+                     <div></div>
+                </div>
+              </div>
             </section>
         </div>
         </>
