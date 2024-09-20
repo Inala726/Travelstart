689d019d24bbdbf059143b25e33e5362edb30e73
Daniel24356 <igwedaniel128@gmail.com>
2024-09-20 05:47:08 -0700

    add new files

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import "./App.css"
 import React from 'react'
 import TripCatalog from './pages/TripCatalog/TripCatalog'
 import Great from "./pages/Great"
+import Accomodations from "./pages/Accomodation"
 
 const App = () => {
   return (
     <>
 
-    <div className="MAIN">
-    <TripCatalog/>
-    </div>
     {/* <div className="MAIN">
-    <Great/>
+    <TripCatalog/>
     </div> */}
+    <div className="MAIN">
+    <Great/>
+    <Accomodations/>
+    </div>
  
     </>
   )
