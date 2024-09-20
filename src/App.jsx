369d663d12bc6e87da9a95347f024d8c15bde24c
Inala <inalajacob740@gmail.com>
2024-09-20 13:22:08 +0100

    sum' shii

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import About from './components/About/About'
 import './App.css'
-import Contact from './components/Contact'
+// import Contact from './components/Contact'
 // import Nav from './Components/Navbar/Nav'
 // import Homepage from './Pages/Homepage/Homepage'
 // import Footer from './Components/Footer/Footer'
