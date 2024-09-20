face09a9dc0f216895f3caee857de6ca1a9e52ad
Emmanuelbagbi <emmanuelbagbi4@gmail.com>
2024-09-20 12:08:12 +0100

    initial commit

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -54,6 +54,9 @@ const Homepage = () => {
               Plan your perfect getaway and embark on new adventures.
               Let us turn your dream vacation into reality
               </div>
+              <div className="buttons">
+                <button className='title-button'>SEE MORE</button>
+              </div>
             </div>
           </div>
         ))}
@@ -76,17 +79,16 @@ const Homepage = () => {
           <div className='info1'>
             <p>Mount Kelimutu is a volcano located 
               on Flores island, NTT Province,indonesia.
-              The location of this mountain is precise in pemo village
-              Lake Matano is an ancient tectonic lake that was formed from the movement of the earth’s crustal plates at the end of the Pliocene.
+              The location of this mountain is precise in pemo village.
             </p>
           </div>
           <div className='info1'>
           There are beaches that have very strong Balinese nuances besides the island of the gods, Bali.
 
-Well, the beach is called Ulung Suso Beach, in Mabonta Village, Burau District, East Luwu.
+            Well, the beach is called Ulung Suso Beach, in Mabonta Village.
           </div>
           <div className='info1'>
-          Two waterfalls that are pleasing to the eye are located in East Luwu Regency, namely the Mata Buntu waterfall and the Kali Dingin waterfall, both located in Wasuponda sub-district, only in different villages. 
+          Two waterfalls that are pleasing to the eye are located in East Luwu Regency, namely the Mata Buntu waterfall and the Kali Dingin waterfall. 
           </div>
     </section>
 
@@ -100,14 +102,17 @@ Well, the beach is called Ulung Suso Beach, in Mabonta Village, Burau District, 
             <span>TOUR 1</span>
             <p>There will be a small</p>
           </div>
+
           <div className='tour2'>
           <span>TOUR 2</span>
           <p>There will be a small</p>
           </div>
+
           <div className='tour3'>
           <span>TOUR 3</span>
           <p>There will be a small</p>
           </div>
+
           <div className='tour4'>
           <span>TOUR 4</span>
           <p>There will be a small</p>
@@ -211,6 +216,102 @@ Well, the beach is called Ulung Suso Beach, in Mabonta Village, Burau District, 
               </div>
             </div>
           </div>
+    </section>
+
+    <section className='whyus'>
+      <div className="whyus-nav">
+        <h2>4 Reasons Why you should visit new zealand</h2>
+      </div>
+      <div className="whyus-card">
+          <div className='whyus-card1'>
+            <div className='whyus-title'>TRIP OF YOUR DREAM</div>
+            <div className='span-split'>
+              <span>1. Breathtaking Scenery - From stunning fjords to rolling hills, New Zealand offers incredible landscapes for every nature lover.</span>
+              <span>2. Adventure Capital - Known for activities like bungee jumping, skydiving, and hiking, it's a paradise for thrill-seekers.</span>
+              <span>3. Rich Māori Culture - Experience authentic Māori traditions, art, and history, adding a cultural depth to your trip.</span>
+              <span>4. Unique Wildlife - Spot native species like the kiwi bird, dolphins, and even penguins in their natural habitats.</span>
+            </div>
+            <div className='whyus-block'>
+              <button className='readmore'>Read More</button>
+            </div>
+          </div>
+          <div className='whyus-card2'>
+            <div className="cards">
+              <div className="card red">
+              </div>
+              <div className="card blue">
+              </div>
+              <div className="card green">
+              </div>
+            </div>
+
+          </div>
+      </div>
+
+    </section>
+
+
+    <section className='last-dimension'>
+          <div className="us">WHY US?</div>
+          <div className="hobbition">
+              <div className="hobbition-info">
+                <h2>Hobbision</h2>
+                <span>Discover incredible destinations and unbeatable travel deals.
+                  Plan your perfect getaway and embark on new adventures.
+                  </span>
+                  <button className='readmore'>Read More</button>
+              </div>
+
+
+              <div className="hobbition-pic">
+               
+            </div>
+          </div>
+
+          <div className="trending">
+              <span><h2>Popular tour</h2></span>
+
+              <div className="tour-data">
+                  <div className="tour-card">
+                    <div className="tc-info">
+                      <span>Paris, France </span>
+                      <div className="tour-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                      </svg></div>
+                    </div>
+                  </div>
+
+
+                  <div className="tour-card1">
+                    <div className="tc-info">
+                      <span>Kyoto, Japan </span>
+                      <div className="tour-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                      </svg></div>
+                    </div>
+                  </div>
+
+                  <div className="tour-card2">
+                    <div className="tc-info">
+                      <span>Santorini, Greece </span>
+                      <div className="tour-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                      </svg></div>
+                    </div>
+                  </div>
+
+
+                  <div className="tour-card3">
+                    <div className="tc-info">
+                      <span>Machu Picchu, Peru  </span>
+                      <div className="tour-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                      </svg></div>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          
     </section>
     </div>
   );
