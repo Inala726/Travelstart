74cd5b4adbcbe599816d59ae9d2253469b54b5da
Inala <inalajacob740@gmail.com>
2024-09-22 00:34:59 +0100

    removed great

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Great from './Pages/Great'
+// import Great from './Pages/Great'
 import About from "./Pages/About/About";
 import Contact from './Pages/Contact/Contact';
 import Homepage from "./Pages/Homepage/Homepage";
@@ -17,7 +17,7 @@ const App = () => {
           <Route path="/" element={<Homepage />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/countries" element={<Great />} />
+          {/* <Route path="/countries" element={<Great />} /> */}
           <Route path="/tc" element={<TripCatalog />} />
         </Routes>
       </Router>
